b36e661c9a71be39a0d1bcc62bfbbef5d893d906
amney14 <Amney@Amneys-MacBook-Pro.local>
2015-06-11 18:00:10 -0400

    MichMediTrans iOS App

---
+++ MichMediTrans_iOS/MichMediTrans_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MichMediTrans_iOS
+//
+//  Created by Amney Iskandar on 6/11/15.
+//  Copyright (c) 2015 MichMediTrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
