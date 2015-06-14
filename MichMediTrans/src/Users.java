f9d3a683a143253a1579d201b5ec0141f4bb98a9
amney14 <Amney@Amneys-MacBook-Pro.local>
2015-06-14 14:32:01 -0400

    Users java file
    Database fields for each user that signs up

---
+++ MichMediTrans/src/Users.java
@@ -0,0 +1,109 @@
+
+public class Users {
+	
+	
+	private String FirstName;
+	private String LastName;
+	private String Email;
+	private String UserName;
+	private String Password;
+	private String Phone;
+	private String AccountNum;
+	
+	public  String getFirstName()
+	{
+		return FirstName;
+		
+	}
+	
+	public void setFirstName(String FirstName)
+	
+	{
+		this.FirstName = FirstName;
+		
+	}
+	
+	public  String getLastName()
+	{
+		return LastName;
+		
+	}
+	
+	public void setLastName(String LastName)
+	
+	{
+		this.LastName = LastName;
+		
+	}
+	
+	public  String getEmail()
+	{
+		return Email;
+		
+	}
+	
+	public void setEmail(String Email)
+	
+	{
+		this.Email = Email;
+		
+	}
+	
+	public  String getUserName()
+	{
+		return UserName;
+		
+	}
+	
+	public void setUserName(String UserName)
+	
+	{
+		this.UserName = UserName;
+		
+	}
+	
+	public  String getPassword()
+	{
+		return Password;
+		
+	}
+	
+	public void setPassword(String Password)
+	
+	{
+		this.Password = Password;
+		
+	}
+	public  String getPhone()
+	{
+		return Phone;
+		
+	}
+	
+	public void setPhone(String Phone)
+	
+	{
+		this.Phone = Phone;
+		
+	}
+	
+	public  String getAccountNum()
+	{
+		return Phone;
+		
+	}
+	
+	public void setAccountNum(String AccountNum)
+	
+	{
+		this.AccountNum = AccountNum;
+		
+	}
+
+
+
+
+
+	
+
+}
