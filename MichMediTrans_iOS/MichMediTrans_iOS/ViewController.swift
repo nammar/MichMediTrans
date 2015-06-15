6dbb26b7a67fda96bee6297da05e055da1598883
Anthony Blackmon <blackmon.anthony1@gmail.com>
2015-06-15 00:19:53 -0400

    Added Sign-In/Sign-Up Screen for iOS
    Added Logo, Sign In/Sign Up Button. Functionality still needs to be
    added.

--- MichMediTrans_iOS/MichMediTrans_iOS/ViewController.swift
+++ MichMediTrans_iOS/MichMediTrans_iOS/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBAction func signIn(sender: AnyObject) {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signUp(sender: AnyObject) {
     }
-
 
 }
 
