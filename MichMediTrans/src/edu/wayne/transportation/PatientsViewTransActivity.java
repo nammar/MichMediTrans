04ceb11f98dc87fc3c9ae5c3b766e341748ba4bd
julliank15 <ay2120@wayne.edu>
2015-06-15 14:54:13 -0400

    Patient transportation view updated
    1 spinner widget for company choice, 1 spinner widget for transportation
    type and a submit button added to .xml. Alert dialog for submit button
    functionality  added to .java for user to check there input.

--- MichMediTrans/src/edu/wayne/transportation/PatientsViewTransActivity.java
+++ MichMediTrans/src/edu/wayne/transportation/PatientsViewTransActivity.java
@@ -4,18 +4,64 @@ import edu.wayne.michmeditrans.R;
 import edu.wayne.michmeditrans.R.id;
 import edu.wayne.michmeditrans.R.layout;
 import edu.wayne.michmeditrans.R.menu;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class PatientsViewTransActivity extends Activity {
+	
+	// ----------------Declare Widget variables----------------//
+		private Spinner spCompanies, spTransportType;
+		private Button btSubmit;
+		
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_patients_view_trans);
+		
+		//Define Widget variables
+		spCompanies = (Spinner) findViewById(R.id.spCompanies);
+		spTransportType = (Spinner) findViewById(R.id.spTransportType);
+		btSubmit = (Button) findViewById(R.id.btSubmit); 
+		
+		
+		//Define Submit Button
+		btSubmit.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try {
+				
+					
+					String companyChoice = String.valueOf(spCompanies.getSelectedItem());
+					String TransTypeChoice = String.valueOf(spTransportType.getSelectedItem());
+					
+					//alert dialog checking users input
+					AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+					builder.setTitle("Transportation Choice");
+					builder.setMessage("Company: " + companyChoice + "\n"
+								        + "Transportation Type: " + TransTypeChoice + "\nAre You Sure?");
+
+					//Accept user input or not-
+					//start new activity
+					builder.setPositiveButton("Yes",null);   ////-------Add code to start activity here in positive button-------//
+					builder.setNegativeButton("No",null);
+					builder.show();
+					
+				} catch (NumberFormatException e) {
+					System.out.println("Error: A input field is blank.");
+				}
+
+			}
+		});
 	}
 
 	@Override
