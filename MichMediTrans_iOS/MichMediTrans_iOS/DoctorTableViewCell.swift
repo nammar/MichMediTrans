7fface6599a144e78e7bade4fa93222053069702
Nariman Ammar <nammar@wayne.edu>
2015-06-22 13:57:29 -0400

    added screen to show details of an individual doctor
    -currently just screen without controller file behind
    -made sign in screen entry point
    -navigation route: sing in as patient button -> doctors screen -> click
    any cell in the list -> individual doctor details

--- MichMediTrans_iOS/MichMediTrans_iOS/DoctorTableViewCell.swift
+++ MichMediTrans_iOS/MichMediTrans_iOS/DoctorTableViewCell.swift
@@ -12,7 +12,7 @@ class DoctorTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
-    @IBOutlet weak var ratingControl: RatingControl!
+  
     
     override func awakeFromNib() {
         super.awakeFromNib()
